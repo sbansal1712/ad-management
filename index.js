1e0a6d58baa99d1016e0e48a319861b473fac13f
sbansal1712 <Shubham1.Bansal@ril.com>
2020-07-12 02:56:41 +0530

    get API added

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require("express");
 const path = require("path");
-var cors = require("cors");
 const PORT = process.env.PORT || 5000;
-var app = express();
+require("./database/connect_mongo");
 const routes = require("./routes/route");
 
-require("./database/connect_mongo");
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-var corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders:
-    "Content-Type,X-Requested-With,authorization,accesstoken, sessionid",
-};
-app.use(routes);
-app.use(cors(corsOptions));
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .use(routes)
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
