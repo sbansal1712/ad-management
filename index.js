36daca649ba862e09510ba713426063680ca6455
sbansal1712 <Shubham1.Bansal@ril.com>
2020-07-12 02:41:39 +0530

    get API added

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require("./database/connect_mongo");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 var corsOptions = {
   origin: "*",
